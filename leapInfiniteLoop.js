96a379ca493dca5662fabd7dd82c6285862279e5
David Matthews <David.Matthews.1@uvm.edu>
2020-09-06 11:56:19 -0400

    through step 65 del 1

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -3,21 +3,31 @@ var controllerOptions = {};
 var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 
+function HandleFinger(finger) {
+    console.log(finger);
+    console.log(finger.tipPosition);
+}
 
-Leap.loop(controllerOptions, function(frame){
-    if (frame.hands.length == 1){
-        var hand = frame.hands[0];
-        var fingers = hand.fingers;
-        for (var i = 0; i < fingers.length; i++)
-            if (fingers[i].type == 1) {
-            console.log(fingers[i]);
-            }
+function HandleHand(hand) {
+    var fingers = hand.fingers;
+    for (var i = 0; i < fingers.length; i++) {
+        if (fingers[i].type == 1) {
+            HandleFinger(fingers[i]);
         }
-        // console.log(fingers);
+    }
+}
+
+function HandleFrame(frame) {
+    if (frame.hands.length == 1) {
+        var hand = frame.hands[0];
+        HandleHand(hand);
+    }
     // clear();
     // x += Math.floor(Math.random() * 3) - 1;
     // y += Math.floor(Math.random() * 3) - 1;
     //
     // circle(x, y, 50);
+
 }
-);
+
+Leap.loop(controllerOptions, HandleFrame);

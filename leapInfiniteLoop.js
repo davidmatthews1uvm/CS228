dbbacde5983b19e290de65c259c944cf7c428c4d
David Matthews <David.Matthews.1@uvm.edu>
2020-09-06 12:08:54 -0400

    step 70 del 1

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -3,13 +3,31 @@ var controllerOptions = {};
 var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 
+var rawXMin = 0;
+var rawXMax = 1;
+var rawYMin = 0;
+var rawYMax = 1;
+
+function UpdateBounds(x,y,z) {
+    if (x < rawXMin) {
+        rawXMin = x;
+    } else if (x > rawXMax) {
+        rawXMax = x;
+    }
+    if (y < rawYMin) {
+        rawYMin = y;
+    } else if (y > rawXMax) {
+        rawYMax = y;
+    }
+}
 function HandleFinger(finger) {
     // console.log(finger);
     // console.log(finger.tipPosition);
     var x, y, z;
     [x, y, z] = finger.tipPosition;
+    UpdateBounds(x, y, z);
 
-    circle(x, y, 50);
+    circle(x, window.innerHeight/2 - y, 50);
 
 }
 

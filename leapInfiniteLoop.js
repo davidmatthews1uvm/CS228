413a20c437eb835f2a452c7cd0731a2251e22958
David Matthews <David.Matthews.1@uvm.edu>
2020-09-06 11:36:16 -0400

    step 53 del 1

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,10 +5,11 @@ var y = window.innerHeight/2;
 
 
 Leap.loop(controllerOptions, function(frame){
-    clear();
-    x += Math.floor(Math.random() * 3) - 1;
-    y += Math.floor(Math.random() * 3) - 1;
-
-    circle(x, y, 50);
+    console.log(frame);
+    // clear();
+    // x += Math.floor(Math.random() * 3) - 1;
+    // y += Math.floor(Math.random() * 3) - 1;
+    //
+    // circle(x, y, 50);
 }
 );

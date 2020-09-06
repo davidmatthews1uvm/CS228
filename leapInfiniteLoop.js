2f32fc0f04722c57ce8fb572185b359ceaf533b2
David Matthews <David.Matthews.1@uvm.edu>
2020-09-06 11:43:06 -0400

    through step 59 del 1

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,7 +5,11 @@ var y = window.innerHeight/2;
 
 
 Leap.loop(controllerOptions, function(frame){
-    console.log(frame);
+    if (frame.hands.length == 1){
+        var hand = frame.hands[0];
+        var fingers = hand.fingers
+        console.log(fingers);
+    }
     // clear();
     // x += Math.floor(Math.random() * 3) - 1;
     // y += Math.floor(Math.random() * 3) - 1;

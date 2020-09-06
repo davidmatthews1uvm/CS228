9656580afafd9a202c6427646408a17fecbbfeb1
David Matthews <David.Matthews.1@uvm.edu>
2020-09-06 11:50:32 -0400

    step 61 del 1

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -7,9 +7,13 @@ var y = window.innerHeight/2;
 Leap.loop(controllerOptions, function(frame){
     if (frame.hands.length == 1){
         var hand = frame.hands[0];
-        var fingers = hand.fingers
-        console.log(fingers);
-    }
+        var fingers = hand.fingers;
+        for (var i = 0; i < fingers.length; i++)
+            if (fingers[i].type == 1) {
+            console.log(fingers[i]);
+            }
+        }
+        // console.log(fingers);
     // clear();
     // x += Math.floor(Math.random() * 3) - 1;
     // y += Math.floor(Math.random() * 3) - 1;

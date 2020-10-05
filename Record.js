43ffd9986b1fe2cbe74c3f35e3cf7573ebcd4e9b
David Matthews <David.Matthews.1@uvm.edu>
2020-10-05 12:19:01 -0400

    screenshot A complete

--- Record.js
+++ Record.js
@@ -1,3 +1,5 @@
+nj.config.printThreshold = 1000;
+
 var controllerOptions = {};
 
 var x = window.innerWidth/2;
@@ -10,7 +12,9 @@ var rawYMax = 300;
 
 var previousNumHands = 0;
 var currentNumHands = 0;
-var oneFrameOfData = nj.zeros([5,4,6]);
+var numSamples = 2;
+var currentSample = 0;
+var framesOfData = nj.zeros([5, 4, 6, numSamples]);
 
 
 function UpdateBounds(x, y, z) {
@@ -27,9 +31,8 @@ function UpdateBounds(x, y, z) {
 }
 
 function TransformedCoordinates(x, y) {
-    UpdateBounds(x, y);
-    var scaledX  = ((x - rawXMin) / (rawXMax - rawXMin) ) * window.innerWidth;
-    var scaledY  = window.innerHeight - ((y - rawYMin) / (rawYMax - rawYMin) ) * window.innerHeight;
+    var scaledX  = x * window.innerWidth;
+    var scaledY  = (1 - y) * window.innerHeight;
     return [scaledX, scaledY];
 }
 
@@ -47,21 +50,24 @@ function DrawLine(x1, y1, x2, y2, weight, color) {
 }
 
 
-function HandleBone(bone, finger_idx) {
-    [x1, y1, z1] = bone.prevJoint;
-    [x2, y2, z2] = bone.nextJoint;
+function HandleBone(bone, finger_idx, interaction_box) {
+    normalizedPrevJoint = interaction_box.normalizePoint(bone.prevJoint, true);
+    normalizedNextJoint = interaction_box.normalizePoint(bone.nextJoint, true);
+
+    [x1, y1, z1] = normalizedPrevJoint;
+    [x2, y2, z2] = normalizedNextJoint;
 
     [xs1, ys1] = TransformedCoordinates(x1, y1);
     [xs2, ys2] = TransformedCoordinates(x2, y2);
 
 
 
-    oneFrameOfData.set(finger_idx, bone.type, 0, xs1);
-    oneFrameOfData.set(finger_idx, bone.type, 1, ys1);
-    oneFrameOfData.set(finger_idx, bone.type, 2, z1);
-    oneFrameOfData.set(finger_idx, bone.type, 3, xs2);
-    oneFrameOfData.set(finger_idx, bone.type, 4, ys2);
-    oneFrameOfData.set(finger_idx, bone.type, 5, z2);
+    framesOfData.set(finger_idx, bone.type, 0, currentSample, x1);
+    framesOfData.set(finger_idx, bone.type, 1, currentSample, y1);
+    framesOfData.set(finger_idx, bone.type, 2, currentSample, z1);
+    framesOfData.set(finger_idx, bone.type, 3, currentSample, x2);
+    framesOfData.set(finger_idx, bone.type, 4, currentSample, y2);
+    framesOfData.set(finger_idx, bone.type, 5, currentSample, z2);
 
     color = [0,0,0]
     if (currentNumHands == 1) {
@@ -70,7 +76,7 @@ function HandleBone(bone, finger_idx) {
         color = [(4-bone.type)*40, 0, 0];
     }
 
-    DrawLine(x1, y1, x2, y2, 2*(4 - bone.type),  color);
+    DrawLine(x1, y1, x2, y2, 10*(4 - bone.type),  color);
 
 
 }
@@ -82,31 +88,38 @@ function HandleFinger(finger) {
     }
 }
 
-function HandleHand(hand) {
+function HandleHand(hand, interaction_box) {
     var fingers = hand.fingers;
     for (var bone_idx = 3; bone_idx >=0; bone_idx -= 1) {
         for (var finger_idx = 0; finger_idx < fingers.length; finger_idx++) {
-             HandleBone(fingers[finger_idx].bones[bone_idx], finger_idx);
+             HandleBone(fingers[finger_idx].bones[bone_idx], finger_idx, interaction_box);
         }
     }
 
 }
 
-function RecordData() {
-    background(0);
+function RecordData(previousNumHands, currentNumHands) {
+    if (previousNumHands == 1 && currentNumHands == 2 || previousNumHands == 2 && currentNumHands == 1) {
+            background(0);
+            if (currentNumHands == 1) {
+                console.log(framesOfData.toString());
+            }
+    }
+    if (currentNumHands == 2) {
+        currentSample = (currentSample+1)%numSamples;
+
+    }
+
 }
 
 function HandleFrame(frame) {
     currentNumHands = frame.hands.length;
     if (currentNumHands > 0) {
         var hand = frame.hands[0];
-        HandleHand(hand);
+        HandleHand(hand, frame.interactionBox);
     }
-    if (previousNumHands == 2 && currentNumHands == 1) {
-        RecordData();
-        console.log(oneFrameOfData.toString());
+    RecordData(previousNumHands, currentNumHands);
 
-    }
 
     previousNumHands = currentNumHands;
 }

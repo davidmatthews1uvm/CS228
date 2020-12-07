9d148a30fd739a054461fbdec9df0a3ad4fd9fd6
David Matthews <david.matthews.1@uvm.edu>
2020-12-06 17:54:16 -0800

    wip on new layout

--- ScreenPaintingTesting.js
+++ ScreenPaintingTesting.js
@@ -4,6 +4,14 @@ var currentDigit = getRandomInt(10);
 var y_grid = window.innerHeight / 64;
 var x_grid = window.innerWidth / 64;
 
+var beginTime;
+var timePerDigit = 5;
+
+var prevNumHands = 0;
+var currNumHands = 0;
+
+var oneFrameOfData = nj.zeros([5, 4, 6]);
+
 /**
  * x_grid: 3*4 - 12*4
  * y_grid: 0-8
@@ -45,21 +53,67 @@ function DrawTop() {
  * x_grid 0-63
  * y_grid 8-9
  */
-function DrawTimeRemaining(frac) {
+function DrawTimeRemaining(remainTimeFrac) {
     rect_y = 8
     rect_height = 1;
 
-    fill([0,200*frac,0]);
+    fill([0, 100 + 100*remainTimeFrac, 0]);
     strokeWeight(0);
-    rect(0*x_grid, (rect_y)*y_grid, 64*x_grid, rect_height*y_grid);
+    rect(0*x_grid, (rect_y)*y_grid, 64*x_grid*remainTimeFrac, rect_height*y_grid);
 }
 
+function DrawTimeGestureAppear(gestureTimeFrac, color) {
+    triangle_height = 1 * y_grid;
+    half_triangle_width = 0.25 * x_grid;
+    x_pos = gestureTimeFrac * 64 * x_grid; // top point
+    y_pos = 9 * y_grid;
+    
+    fill(color);
+    strokeWeight(0);
+    triangle(x_pos, y_pos,
+            x_pos - half_triangle_width, y_pos + triangle_height,
+            x_pos + half_triangle_width, y_pos + triangle_height);
+}
+
+
+function DrawSectionLines() {
+    x_pos = 32 * x_grid;
+    y_pos_top = 9 * y_grid;
+    y_pos_bottom = 64 * y_grid;
+    DrawLine(0, y_pos_top, x_grid * 64, y_pos_top, 3, 0);
+    DrawLine(x_pos, y_pos_top,
+            x_pos, y_pos_bottom,
+            3, 0);
+}
+
+function SwitchDigit() {
+    beginTime = new Date();
+    currentDigit = getRandomInt(10);
+}
+
+
 function HandleFrame(frame) {
-    if (frame.hands.length == 1) {
-        var hand = frame.hands[0];
-        DrawTop();
-        DrawTimeRemaining(0.7);
+    currNumHands = frame.hands.length;
+    if (currNumHands == 1 && prevNumHands == 0) {
+       SwitchDigit();
     }
+    if (currNumHands == 1) {
+        var hand = frame.hands[0];
+        timeRemainFrac = getTimeFracRemaining(beginTime, timePerDigit);
+
+        DrawTop();
+        DrawSectionLines();
+        DrawTimeRemaining(timeRemainFrac);
+        DrawTimeGestureAppear(0.7, [0,0,0]);
+        DrawTimeGestureAppear(0.4, [170, 0, 170]);
+
+        DrawHand(frame, timeRemainFrac);
+
+        if (timeRemainFrac <= 0) {
+            SwitchDigit();
+        }
+    }
+    prevNumHands = currNumHands;
 }
 
 Leap.loop(controllerOptions, function(frame){
--- utils.js
+++ utils.js
@@ -2,3 +2,74 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+function getTimeDiffSeconds(prevTime, currTime){
+    timeDifferenceInMs = currTime - prevTime;
+    timeDifferenceInS = timeDifferenceInMs/1000;
+    return timeDifferenceInS;
+}
+
+function getTimeFracRemaining(prevTime, totalTime) {
+    return 1 - Math.max(getTimeDiffSeconds(prevTime, new Date())/totalTime, 0);
+}
+
+
+function DrawLine(x1, y1, x2, y2, weight, color) {
+    [x1, y1] = TransformedCoordinatesHand(x1, y1);
+    [x2, y2] = TransformedCoordinatesHand(x2, y2);
+    colorMode(RGB);
+    strokeWeight(weight);
+    stroke(color);
+    line(x1, y1, x2, y2);
+}
+
+// Hand Drawing
+
+function TransformedCoordinatesHand(x, y) {
+    var scaledX  = x * window.innerWidth/2;
+    var scaledY  = (1 - y) * window.innerHeight/2;
+    return [scaledX, scaledY];
+}
+
+function HandleBone(bone, finger_idx, interaction_box, acc) {
+    normalizedPrevJoint = interaction_box.normalizePoint(bone.prevJoint, true);
+    normalizedNextJoint = interaction_box.normalizePoint(bone.nextJoint, true);
+
+    [x1, y1, z1] = normalizedPrevJoint;
+    [x2, y2, z2] = normalizedNextJoint;
+
+    [xs1, ys1] = TransformedCoordinatesHand(x1, y1);
+    [xs2, ys2] = TransformedCoordinatesHand(x2, y2);
+
+
+
+    oneFrameOfData.set(finger_idx, bone.type, 0, x1);
+    oneFrameOfData.set(finger_idx, bone.type, 1, y1);
+    oneFrameOfData.set(finger_idx, bone.type, 2, z1);
+    oneFrameOfData.set(finger_idx, bone.type, 3, x2);
+    oneFrameOfData.set(finger_idx, bone.type, 4, y2);
+    oneFrameOfData.set(finger_idx, bone.type, 5, z2);
+
+    color = [(2-bone.type/2)*125*(1 - acc),
+             (2-bone.type/2)*125*(acc), 0];
+
+    DrawLine(x1, y1, x2, y2, 10*(4 - bone.type),  color);
+
+
+}
+
+
+
+function HandleHand(hand, interaction_box, acc) {
+    var fingers = hand.fingers;
+    for (var bone_idx = 3; bone_idx >=0; bone_idx -= 1) {
+        for (var finger_idx = 0; finger_idx < fingers.length; finger_idx++) {
+             HandleBone(fingers[finger_idx].bones[bone_idx], finger_idx, interaction_box, acc);
+        }
+    }
+
+}
+
+function DrawHand(frame, acc) {
+    var hand = frame.hands[0];
+    HandleHand(hand, frame.interactionBox, acc);
+}
